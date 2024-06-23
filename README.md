7ecdc66b33063df884a71dbc542b2d87f3518671
Carlos Monteiro <cmmonteiro40@gmail.com>
2024-06-23 15:42:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# What is this project?
+Vai analisar um documento de texto e verificar Regular expressions(Regex) e Sentiment Analysis
---
+++ main.py
@@ -0,0 +1,3 @@
+# É melhor fazer a análise no python ou no jupyter?
+# A resposta é jupyter, pois nos oferece uma estrutura melhor com células e permite uma análise mais eficiente.
+# Então, neste terminal, escreve: jupyter-lab
